6459897d5603ecdf68a7d642e495e4871b51e742
zehrakorkusuz <zehra.korkusuz@outlook.com>
2021-03-28 22:04:20 +0300

    services updated card version

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def home():
     return render_template('index.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
 
